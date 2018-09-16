3bf0fce12b9b71ce04d6d956df42fd51399c90d9
Rich <crayonsrcool@hotmail.com>
2018-09-16 14:33:04 -0700

    Refactored services to extend a base service

---
+++ pet-clinic-data/src/main/java/deavila/richard/petclinic/service/CrudService.java
@@ -0,0 +1,16 @@
+package deavila.richard.petclinic.service;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    T findById(ID id);
+
+    Set<T> findAll();
+
+    T save(T object);
+
+    void delete (T object);
+
+    void deleteById (ID id);
+}
--- pet-clinic-data/src/main/java/deavila/richard/petclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/deavila/richard/petclinic/service/PetService.java
@@ -2,13 +2,7 @@ package deavila.richard.petclinic.service;
 
 import deavila.richard.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
