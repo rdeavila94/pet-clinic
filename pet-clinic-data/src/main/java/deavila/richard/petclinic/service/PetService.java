cf12440c6fd8244d2ac2a7aa97ad047d3821f259
Rich <crayonsrcool@hotmail.com>
2018-09-16 12:28:01 -0700

    Implement interfaces for services

---
+++ pet-clinic-data/src/main/java/deavila/richard/petclinic/service/PetService.java
@@ -0,0 +1,14 @@
+package deavila.richard.petclinic.service;
+
+import deavila.richard.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
